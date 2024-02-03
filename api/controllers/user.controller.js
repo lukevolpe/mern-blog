9b61006af63599aebf621421dfd11fa38bb007a0
Luke Volpe <luke.volpe@gmail.com>
2024-02-03 23:13:15 +0000

    User / auth routes / controllers

---
+++ api/controllers/user.controller.js
@@ -0,0 +1,4 @@
+// Test API route
+export const test = (req, res) => {
+  res.json({ message: 'API test route OK' });
+};
--- api/index.js
+++ api/index.js
@@ -1,7 +1,10 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import 'dotenv/config';
+import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
 
+// Connects to MongoDB and logs whether this has worked or if there is an error.
 mongoose
   .connect(process.env.MONGO_DB)
   .then(() => {
@@ -13,6 +16,12 @@ mongoose
 
 const app = express();
 
+// Allows JSON to be sent to the API
+app.use(express.json());
+
 app.listen(3000, () => {
   console.log('Server is running on port 3000!');
 });
+
+app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
