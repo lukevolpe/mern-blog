61ea456de71acb72550a9e92c0156f5c587e2c0b
Luke Volpe <luke.volpe@gmail.com>
2024-02-17 12:56:49 +0000

    added posts section to dashboard

---
+++ client/src/components/DashPosts.jsx
@@ -0,0 +1,9 @@
+const DashPosts = () => {
+  return (
+    <div>
+      <h1>Dash Posts</h1>
+    </div>
+  );
+};
+
+export default DashPosts;
--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,14 +1,16 @@
 import { Button, Modal, Sidebar } from 'flowbite-react';
 import { useEffect, useState } from 'react';
-import { HiArrowSmRight, HiUser } from 'react-icons/hi';
+import { HiArrowSmRight, HiDocumentText, HiUser } from 'react-icons/hi';
 import { Link, useLocation } from 'react-router-dom';
 import { HiOutlineExclamationCircle } from 'react-icons/hi';
 import { signOutSuccess } from '../redux/user/userSlice.js';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 const DashSidebar = () => {
   const location = useLocation();
   const dispatch = useDispatch();
+
+  const { currentUser } = useSelector((state) => state.user);
 
   const [tab, setTab] = useState('');
   const [showSignOutModal, setShowSignOutModal] = useState(false);
@@ -41,12 +43,12 @@ const DashSidebar = () => {
     <>
       <Sidebar className='w-full md:w-56'>
         <Sidebar.Items>
-          <Sidebar.ItemGroup>
+          <Sidebar.ItemGroup className='flex flex-col gap-0.5'>
             <Link to='/dashboard?tab=profile'>
               <Sidebar.Item
                 active={tab === 'profile'}
                 icon={HiUser}
-                label={'User'}
+                label={currentUser.isAdmin ? 'Admin' : 'User'}
                 labelColor='dark'
                 className='text-sm'
                 as='div'
@@ -54,6 +56,17 @@ const DashSidebar = () => {
                 Profile
               </Sidebar.Item>
             </Link>
+            {currentUser.isAdmin && (
+              <Link to='/dashboard?tab=posts'>
+                <Sidebar.Item
+                  active={tab === 'posts'}
+                  icon={HiDocumentText}
+                  as='div'
+                >
+                  Posts
+                </Sidebar.Item>
+              </Link>
+            )}
             <Sidebar.Item
               icon={HiArrowSmRight}
               className='cursor-pointer text-sm'
