7be0e1ed4c6d2cbacd77c48aba74bade2cf0db63
Luke Volpe <luke.volpe@gmail.com>
2024-02-17 21:31:51 +0000

    added users table

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,6 +1,11 @@
 import { Button, Modal, Sidebar } from 'flowbite-react';
 import { useEffect, useState } from 'react';
-import { HiArrowSmRight, HiDocumentText, HiUser } from 'react-icons/hi';
+import {
+  HiArrowSmRight,
+  HiDocumentText,
+  HiOutlineUserGroup,
+  HiUser,
+} from 'react-icons/hi';
 import { Link, useLocation } from 'react-router-dom';
 import { HiOutlineExclamationCircle } from 'react-icons/hi';
 import { signOutSuccess } from '../redux/user/userSlice.js';
@@ -56,15 +61,26 @@ const DashSidebar = () => {
               </Sidebar.Item>
             </Link>
             {currentUser.isAdmin && (
-              <Link to='/dashboard?tab=posts'>
-                <Sidebar.Item
-                  active={tab === 'posts'}
-                  icon={HiDocumentText}
-                  as='div'
-                >
-                  Posts
-                </Sidebar.Item>
-              </Link>
+              <>
+                <Link to='/dashboard?tab=posts'>
+                  <Sidebar.Item
+                    active={tab === 'posts'}
+                    icon={HiDocumentText}
+                    as='div'
+                  >
+                    Posts
+                  </Sidebar.Item>
+                </Link>
+                <Link to='/dashboard?tab=users'>
+                  <Sidebar.Item
+                    active={tab === 'users'}
+                    icon={HiOutlineUserGroup}
+                    as='div'
+                  >
+                    Users
+                  </Sidebar.Item>
+                </Link>
+              </>
             )}
             <Sidebar.Item
               icon={HiArrowSmRight}
---
+++ client/src/components/DashUsers.jsx
@@ -0,0 +1,144 @@
+import { Button, Modal, Table } from 'flowbite-react';
+import { useEffect, useState } from 'react';
+import { HiOutlineExclamationCircle } from 'react-icons/hi';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+
+const DashUsers = () => {
+  const { currentUser } = useSelector((state) => state.user);
+  const [users, setUsers] = useState([]);
+  const [showMore, setShowMore] = useState(true);
+  const [showModal, setShowModal] = useState(false);
+  const [userIdToDelete, setUserIdToDelete] = useState(null);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const res = await fetch(`/api/user/get-users`);
+        const data = await res.json();
+        if (res.ok) {
+          setUsers(data.users);
+          if (data.users.length < 10) {
+            setShowMore(false);
+          }
+        }
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    if (currentUser.isAdmin) {
+      fetchUsers();
+    }
+  }, [currentUser._id]);
+
+  const handleShowMore = async () => {
+    const startIndex = users.length;
+    try {
+      const res = await fetch(`/api/user/get-users?startIndex=${startIndex}`);
+      const data = await res.json();
+      if (res.ok) {
+        setUsers((prev) => [...prev, ...data.users]);
+        if (data.users.length < 10) {
+          setShowMore(false);
+        }
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  const handleDeleteUser = async () => {};
+
+  return (
+    <div className='table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500'>
+      {currentUser.isAdmin & (users.length > 0) ? (
+        <>
+          <Table hoverable className='shadow-md'>
+            <Table.Head>
+              <Table.HeadCell>Date Created</Table.HeadCell>
+              <Table.HeadCell>User Image</Table.HeadCell>
+              <Table.HeadCell>Username</Table.HeadCell>
+              <Table.HeadCell>Email</Table.HeadCell>
+              <Table.HeadCell>Admin</Table.HeadCell>
+              <Table.HeadCell>Delete</Table.HeadCell>
+              <Table.HeadCell>
+                <span>Edit</span>
+              </Table.HeadCell>
+            </Table.Head>
+            {users.map((user) => (
+              <Table.Body className='divide-y'>
+                <Table.Row className='bg-white dark:border-gray-700 dark:bg-gray-800'>
+                  <Table.Cell>
+                    {new Date(user.createdAt).toLocaleString()}
+                  </Table.Cell>
+                  <Table.Cell>
+                    <img
+                      src={user.profilePhoto}
+                      alt={user.username}
+                      className='w-10 h-10 object-cover bg-gray-500 rounded-full mx-auto'
+                    />
+                  </Table.Cell>
+                  <Table.Cell>{user.username}</Table.Cell>
+                  <Table.Cell>{user.email}</Table.Cell>
+                  <Table.Cell>{user.isAdmin}</Table.Cell>
+                  <Table.Cell>
+                    <span
+                      onClick={() => {
+                        setShowModal(true);
+                        setUserIdToDelete(user._id);
+                      }}
+                      className='font-medium text-red-500 hover:underline cursor-pointer'
+                    >
+                      Delete
+                    </span>
+                  </Table.Cell>
+                  <Table.Cell>
+                    <Link className='text-teal-500 hover:underline'>
+                      <span>Edit</span>
+                    </Link>
+                  </Table.Cell>
+                </Table.Row>
+              </Table.Body>
+            ))}
+          </Table>
+          {showMore && (
+            <button
+              onClick={handleShowMore}
+              className='w-full text-teal-500 self-center text-sm py-7'
+            >
+              Show more
+            </button>
+          )}
+        </>
+      ) : (
+        <p>You have no users</p>
+      )}
+      <Modal
+        show={showModal}
+        onClose={() => setShowModal(false)}
+        popup
+        size='md'
+      >
+        <Modal.Header />
+        <Modal.Body>
+          <div className='text-center'>
+            <HiOutlineExclamationCircle className='h-14 w-14 text-gray-400 dark:text-gray-200 mb-4 mx-auto' />
+            <h3 className='mb-5 text-lg text-gray-500 dark:text-gray-400'>
+              Are you sure you want to delete this user?
+            </h3>
+            <div className='flex justify-center gap-4'>
+              <Button color='failure' onClick={handleDeleteUser}>
+                Yes, I'm sure
+              </Button>
+              <Button color='gray' onClick={() => setShowModal(false)}>
+                No, cancel
+              </Button>
+            </div>
+          </div>
+        </Modal.Body>
+      </Modal>
+    </div>
+  );
+};
+
+export default DashUsers;
